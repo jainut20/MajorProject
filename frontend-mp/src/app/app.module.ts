e949bc2c8a02e78e5524732c76d942594d71db18
Utkarsh Jain <ujain@buyerfolio.ai>
2022-04-18 13:32:43 +0530

    Drag and Drop videos

--- frontend-mp/src/app/app.module.ts
+++ frontend-mp/src/app/app.module.ts
@@ -8,19 +8,39 @@ import { FooterComponent } from './footer/footer.component';
 import { ContainerComponent } from './container/container.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { FileDragNDropDirective } from './file-drag-n-drop.directive';
+import { DialogConfirmComponent } from './dialog-confirm/dialog-confirm.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ContainerComponent
+    ContainerComponent,
+    FileDragNDropDirective,
+    DialogConfirmComponent
   ],
   imports: [
     FormsModule,
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatIconModule, 
+    MatInputModule, 
+    MatListModule, 
+    MatButtonModule, 
+    MatGridListModule, 
+    MatSnackBarModule, 
+    BrowserAnimationsModule, 
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend-mp/src/app/container/container.component.ts
+++ frontend-mp/src/app/container/container.component.ts
@@ -1,30 +1,64 @@
 import { Component, OnInit } from '@angular/core';
 import { UploadService } from '../upload.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogConfirmComponent } from '../dialog-confirm/dialog-confirm.component';
 
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
   styleUrls: ['./container.component.css']
 })
+
 export class ContainerComponent implements OnInit {
-  video_file: any
-  constructor(private upload: UploadService) { }
+  public files: any[] = [];
+  video_file: any = null;
+  constructor(private upload: UploadService, private _snackBar: MatSnackBar, public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
-  onFileSelected(event: any) {
-    if (event.target.files.length > 0) {
-      let files: FileList = event.target.files
-      this.video_file = files.item(0);
+  onFileSelected(pFileList: File[]) {
+
+
+    if (pFileList.length > 0) {
+
+      this.files = Object.keys(pFileList).map((key: any) => pFileList[key]);
+      this._snackBar.open("Successfully upload!", 'Close', {
+        duration: 2000,
+      });
+      this.video_file = pFileList[0]
       console.log(this.video_file);
       this.uploadFileToServer()
     }
   }
 
+  openConfirmDialog(pIndex: number): void {
+    const dialogRef = this.dialog.open(DialogConfirmComponent, {
+      panelClass: 'modal-xs'
+    });
+    dialogRef.componentInstance.fName = this.files[pIndex].name;
+    dialogRef.componentInstance.fIndex = pIndex;
+
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result !== undefined) {
+        this.deleteFromArray(result);
+      }
+    });
+  }
+
+  deleteFromArray(index: number) {
+    console.log(this.files);
+    this.files.splice(index, 1);
+  }
+
+
   uploadFileToServer() {
     this.upload.postFile(this.video_file).subscribe(data => {
-      // do something, if upload success
+      this._snackBar.open("Successfully upload!", 'Close', {
+        duration: 2000,
+      });
       console.log(data)
     }, error => {
       console.log(error);
